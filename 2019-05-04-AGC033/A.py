44b9f433b9b84da99b021c0ab8ae13e96d626614
Reo Hirao <reohirao116@gmail.com>
2019-05-05 00:35:15 +0900

    A

--- 2019-05-04-AGC033/A.py
+++ 2019-05-04-AGC033/A.py
@@ -1,30 +1,31 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline()[:-1]
+
+
 h, w = map(int, input().split())
 
 a = [list(str(input())) for i in range(h)]
-edit_list = []
-
+black = []
 for i in range(h):
     for j in range(w):
         if a[i][j] == "#":
-            edit_list.append([i, j])
-pre_edit_list = [x[:] for x in edit_list]
-ans = 0
-while edit_list != []:
-    ans += 1
-    edit_list = []
-    for (i, j) in pre_edit_list:
-        if i-1 >= 0 and a[i-1][j] != "#":
-            a[i-1][j] = "#"
-            edit_list.append([i-1, j])
-        if i+1 <= h-1 and a[i+1][j] != "#":
-            a[i+1][j] = "#"
-            edit_list.append([i+1, j])
-        if j-1 >= 0 and a[i][j-1] != "#":
-            a[i][j-1] = "#"
-            edit_list.append([i, j-1])
-        if j+1 <= w-1 and a[i][j+1] != "#":
-            a[i][j+1] = "#"
-            edit_list.append([i, j+1])
-        pre_edit_list = [x[:] for x in edit_list]
+            black.append([i, j])
 
-print(ans-1)
+ans = 0
+min_ans = 0
+for i in range(h):
+    for j in range(w):
+        min_dis = 10000000
+        if a[i][j] == ".":
+            for (x, y) in black:
+                min_dis = min(abs(x - i) + abs(y - j), min_dis)
+                if min_dis <= min_ans:
+                    break
+            ans = max(ans, min_dis)
+            if min_dis > min_ans:
+                min_ans = min_dis
+
+print(ans)
