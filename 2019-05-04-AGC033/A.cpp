e7a1b80f35963dd309903227994f48b71ca380c0
Reo Hirao <reohirao116@gmail.com>
2019-05-05 02:44:49 +0900

    C++

---
+++ 2019-05-04-AGC033/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int h,w;
+    cin >> h >> w;
+    vector<string> grid(h);
+    for (int i = 0; i < h; i++)
+    {
+        cin >> grid[i];
+    }
+
+};
