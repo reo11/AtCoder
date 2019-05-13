ab82529f4df549256a8cb6fe3097f62488bbf0be
ReoDesktop <reohirao116@gmail.com>
2019-05-13 17:19:58 +0900

    D

--- 2019-05-11-diverta/D.py
+++ 2019-05-11-diverta/D.py
@@ -3,10 +3,11 @@ ans = 0
 if n < 3:
     ans = 0
 else:
-    i = n - 1
-    while i > 1:
+    for i in range(1, int(n**0.5) + 1):
         if n % i == n // i:
             ans += i
-        i -= 1
+        i = (n // i) - 1
+        if n % i == n // i:
+            ans += i
 
 print(ans)
