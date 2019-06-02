1a8981d5b949746f0d7ee4a57547f6d2417b74a4
Reo Hirao <reohirao116@gmail.com>
2019-06-02 13:18:24 +0900

    リファクタ

--- participate/2019_01-06/2019-06-01-M-solutions/D_test.py
+++ participate/2019_01-06/2019-06-01-M-solutions/D_test.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import sys
+sys.setrecursionlimit(20000000)
 connection = defaultdict(lambda: [])
 
 n = int(input())
@@ -10,23 +12,21 @@ for i in range(n-1):
     connection[b].append(a)
     ab.append((a, b))
 
-l = []
-for i in range(1, n+1):
-    l.append([i, len(connection[i])])
-
-l.sort(key=lambda x: x[1], reverse=True)
-
 c = list(map(int, input().split()))
-c.sort(reverse=True)
+c.sort()
 
 ans = [0] * (n+1)
-for i in range(n):
-    ans[l[i][0]] = c[i]
+def dfs(num):
+    ans[num] = c.pop()
+    for next_num in connection[num]:
+        if ans[next_num] == 0 :
+            dfs(next_num)
 
+dfs(1)
 
 score = 0
 for a, b in ab:
     score += min(ans[a], ans[b])
 
 print(score)
-print(" ".join(map(str, c)))
+print(" ".join(map(str, ans[1:])))
