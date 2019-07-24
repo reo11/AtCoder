8932a4ec65b15b2377905398de34463fc90b5eaf
reo hirao <reo@reonoMacBook-Air.local>
2019-07-24 15:42:46 +0900

    ABC061

---
+++ practice/ABC/ABC061-070/abc061/c.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+ab = {}
+for i in range(n):
+    a, b = map(int, input().split())
+    try:
+        ab[a] += b
+    except:
+        ab[a] = b
+count = 0
+ans = 0
+for key in sorted(ab.keys()):
+    count += ab[key]
+    if count >= k:
+        ans = key
+        break
+print(ans)
