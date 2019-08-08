b6bbca88bd0b491d96d7a0db666fc8926626a0c2
Reo Hirao <reohirao116@gmail.com>
2019-08-08 22:16:18 +0900

    abc085 d

---
+++ practice/ABC/ABC081-090/abc085/abc085_d.py
@@ -0,0 +1,25 @@
+import math
+n, h = map(int, input().split())
+
+a = []
+b = []
+for i in range(n):
+    a_, b_ = map(int, input().split())
+    a.append(a_)
+    b.append(b_)
+
+a_max = max(a)
+b.sort(reverse=True)
+
+
+idx = 0
+count = 0
+while h > 0:
+    if idx < n and b[idx] > a_max:
+        h -= b[idx]
+        idx += 1
+    else:
+        count += math.ceil(h / a_max)
+        break
+    count += 1
+print(count)
