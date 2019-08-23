0a10298707e20f446890c1e707879a1b80c1a091
ReoDesktop <reohirao116@gmail.com>
2019-08-23 22:25:43 +0900

    practice

---
+++ practice/ABC/ABC121-130/abc123/b.py
@@ -0,0 +1,16 @@
+import itertools
+from math import ceil
+t = []
+for i in range(5):
+    t.append(int(input()))
+ans = 10**9
+
+for values in list(itertools.permutations(t)):
+    tmp = 0
+    for i, v in enumerate(values):
+        if i == 4:
+            tmp += v
+        else:
+            tmp += (ceil(v / 10) * 10)
+    ans = min(ans, tmp)
+print(ans)
