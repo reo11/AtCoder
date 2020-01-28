0adc94604043df86992c5319fc2de0887ebcdd8c
ReoDesktop <reohirao116@gmail.com>
2020-01-29 03:12:53 +0900

    syouzin

--- practice/ABC/ABC141-150/ABC146/d.py
+++ practice/ABC/ABC141-150/ABC146/d.py
@@ -1,37 +1,24 @@
-from collections import defaultdict
-n = int(input())
-ab = [list(map(int, input())) for _ in range(n)]
+import sys
+sys.setrecursionlimit(500000)
 
-count = [0] * (n+1)
-for a, b in ab:
-    count[a] += 1; count[b] += 1
+N = int(input())
+E = [[] for _ in range(N+1)]
+for i in range(N-1):
+    a, b = map(int, input().split())
+    E[a].append((b, i))
+    E[b].append((a, i))
+K = max(len(e) for e in E)
+print(K)
 
-max_ = 0
-max_num = 0
-for i in range(n+1):
-    if max_ < count[i]:
-        max_num = i
-        max_ = count[i]
-
-d = defaultdict(lambda: [])
-places = defaultdict(lambda: defaultdict(lambda: -1))
-color = defaultdict(lambda: defaultdict(lambda: -1))
-
-for i, (a, b) in enumerate(ab):
-    d[a].append(b)
-    d[b].append(a)
-    places[a][b] = i
-    places[b][a] = i
-
-ans = []
-
-
-ans.append(ab[0])
-a, b = ab[0]
-
-def dfs(cur):
-    for next_ in d[cur]:
-        if color[cur][next_] == -1 and color[next_][cur] == -1:
-            
-            dfs(next_)
-
+ans = [-1] * (N-1)
+def dfs(v=1, p=0, p_col=-1):
+    col = 1
+    for u, idx in E[v]:
+        if u!=p:
+            if col == p_col:
+                col += 1
+            ans[idx] = col
+            dfs(u, v, col)
+            col += 1
+dfs()
+print("\n".join(map(str, ans)))
