b672052bb21dce7f167c3149aba4da5c53754f97
ReoDesktop <reohirao116@gmail.com>
2020-05-04 17:30:25 +0900

    復習

--- participate/2020_04-06/abc164/d.py
+++ participate/2020_04-06/abc164/d.py
@@ -1,21 +1,38 @@
 import sys
 from collections import defaultdict
+def cnt_multiple(s, p):
+    # ABC158E, ABC164D
+    n = len(s)
+    if p != 2 and p != 5:
+        a = []
+        t = 1
+        for i in range(n-1, -1, -1):
+            a.append((ord(s[i])-ord("0")) * t % p)
+            t = t * 10 % p
+        cum = [0]
+        for v in a:
+            cum.append((cum[-1] + v) % p)
+
+        dic = defaultdict(int)
+        ans = 0
+        for v in cum:
+            ans += dic[v]
+            dic[v] += 1
+        return ans
+    elif p == 2:
+        ans = 0
+        for i in range(n):
+            if (ord(s[i]) - ord("0")) % 2 == 0:
+                ans += i+1
+        return ans
+    else:
+        ans = 0
+        for i in range(n):
+            if (ord(s[i]) - ord("0")) % 5 == 0:
+                ans += i+1
+        return ans
 s = str(input())
 n = len(s)
 p = 2019
 
-a = []
-t = 1
-for i in range(n-1, -1, -1):
-    a.append((ord(s[i])-ord("0")) * t % p)
-    t = t * 10 % p
-cum = [0]
-for v in a:
-    cum.append((cum[-1] + v) % p)
-
-dic = defaultdict(int)
-ans = 0
-for v in cum:
-    ans += dic[v]
-    dic[v] += 1
-print(ans)
+print(cnt_multiple(s, p))
--- participate/2020_04-06/abc164/e.py
+++ participate/2020_04-06/abc164/e.py
@@ -1,15 +1,17 @@
 import sys
 from collections import defaultdict
-from math import ceil
+from heapq import heappush, heappop, heapify
 input = sys.stdin.readline
 
-INF = 10**12
+INF = 10**19
 n, m, s = map(int, input().split())
 edges = defaultdict(lambda: defaultdict(lambda: [INF, INF]))
 cd = []
+max_a = 0
 
 for i in range(m):
     u, v, a, b = map(int, input().split())
+    max_a = max(max_a, a)
     edges[u-1][v-1] = [a, b]
     edges[v-1][u-1] = [a, b]
 
@@ -17,56 +19,38 @@ for i in range(n):
     c, d = map(int, input().split())
     cd.append([c, d])
 
-# dfsで今まで訪れた箇所をsetで持っておく
-# 次に行く場所がそこにあれば行かない
-# 経路上の換金できる最小の時間も持っておく
-def dfs(cur_num, goal, route_set, time_trade, cost, silver):
-    if cur_num == goal:
-        return cost, time_trade, silver
-    cnt = 0
-    res = INF
-    final_time_trade = []
-    final_silver = -INF
-    for next_num in edges[cur_num].keys():
-        if next_num in route_set:
-            continue
-        cnt += 1
-        tmp_cost = edges[cur_num][next_num][1]
-        tmp_s = -edges[cur_num][next_num][0]
-        if silver + tmp_s < 0:
-            # 銀が足りない場合の処理
-            min_time = [INF, 0]
-            for c, d in time_trade:
-                times = ceil(-(silver + tmp_s) / c)
-                if times * d < min_time[0] or (times * d == min_time[0] and times*c > min_time[1]):
-                    min_time = [times*d, times*c]
-            tmp_cost += min_time[0]
-            tmp_s += min_time[1]
-        ret = dfs(next_num, goal, route_set | {next_num}, time_trade + [cd[next_num]], cost + tmp_cost, silver + tmp_s)
-        if ret[0] < res:
-            res = ret[0]
-            final_time_trade = ret[1]
-            final_silver = ret[2]
+# 解答ACする
+# Dijakstraの状態を(頂点、所持している銀貨の枚数, cost)で持つ
+SIZE = max_a * n + 1
+# SIZE = 2550
+if s > SIZE:
+    s = SIZE
+nodes = [[INF for _ in range(SIZE+1)] for _ in range(n)]
+nodes[0][s] = 0
 
-    if cnt == 0:
-        return INF, [], -INF
-    else:
-        return res, final_time_trade, final_silver
+def dijakstra():
+    goals = set({0})
+    que = [(0, 0, s)]
+    heapify(que)
+    while 10**6 > len(que) > 0:
+        if len(goals) >= n:
+            break
+        cost, num, silver = heappop(que)
+        if num not in goals:
+            goals.add(num)
+        c, d = cd[num]
+        s_tmp = min(SIZE, silver+c)
+        if silver < SIZE and s_tmp != silver:
+            if nodes[num][s_tmp] > cost + d:
+                heappush(que, (cost + d, num, s_tmp))
+                nodes[num][s_tmp] = min(nodes[num][s_tmp], cost + d)
+        for v in edges[num].keys():
+            a, b = edges[num][v]
+            if silver >= a:
+                if nodes[v][silver-a] > cost + b:
+                    heappush(que, (cost + b, v, silver - a))
+                    nodes[v][silver - a] = min(nodes[v][silver - a], cost + b)
 
-plus_costs = [(0, [cd[0], s])]
+dijakstra()
 for i in range(1, n):
-    plus_costs.append(dfs(0, i, set(), [cd[0]],0 , s))
-
-for i in range(1, n):
-    ans = plus_costs[i][0]
-    for j in range(1, n):
-        for k in range(1, n):
-            # j経由でiまで行く
-            tmp_s = plus_costs[j][2]
-            ret = dfs(j, k, set(), plus_costs[j][1], plus_costs[j][0], tmp_s)
-            tmp_ans = ret[0]
-            tmp_s = ret[2]
-            ret2 = dfs(k, i, set(), ret[1], ret[0], tmp_s)
-            tmp_ans = ret2[0]
-            ans = min(ans, tmp_ans)
-    print(ans)
+    print(min(nodes[i]))
