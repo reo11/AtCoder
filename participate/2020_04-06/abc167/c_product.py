595664eb727076d24f3cdcd9212e22430a531767
ReoDesktop <reohirao116@gmail.com>
2020-05-11 09:30:44 +0900

    復習

---
+++ participate/2020_04-06/abc167/c_product.py
@@ -0,0 +1,27 @@
+from itertools import product
+n, m, x = map(int, input().split())
+ca = [list(map(int, input().split())) for i in range(n)]
+INF = 10**12
+
+ans = INF
+l = [0, 1]
+for v in product(l, repeat=n):
+    rikai = [0 for _ in range(m)]
+    cost = 0
+    for i in range(n):
+        if v[i]:
+            c = ca[i][0]
+            a = ca[i][1:]
+            cost += c
+            for j in range(m):
+                rikai[j] += a[j]
+    f = True
+    for i in range(m):
+        if rikai[i] < x:
+            f = False
+    if f:
+        ans = min(ans, cost)
+if ans == INF:
+    print(-1)
+else:
+    print(ans)
---
+++ participate/2020_04-06/abc167/d_doubling.py
@@ -0,0 +1,15 @@
+# ダブリングを使って解く
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+cur = 0
+pre_doubling = list(range(1, n+1))
+doubling = a[:]
+while k > 0:
+    if k & 1:
+        cur = doubling[cur] - 1
+    k >>= 1
+    pre_doubling = doubling[:]
+    for i in range(n):
+        doubling[i] = pre_doubling[pre_doubling[i]-1]
+print(cur+1)
