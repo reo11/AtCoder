529e526a758429f355a0c79b80b868ec3116799d
reo11 <reohirao116@gmail.com>
2022-10-02 23:27:18 +0900

    abc 241

---
+++ practice/ABC/abc241/a.py
@@ -0,0 +1,6 @@
+a = list(map(int, input().split()))
+
+ans = 0
+for i in range(3):
+  ans = a[ans]
+print(ans)
---
+++ practice/ABC/abc241/b.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+from collections import defaultdict
+
+d = defaultdict(lambda: 0)
+
+for a_i in a:
+  d[a_i] += 1
+
+flag = True
+for b_i in b:
+  if d[b_i] > 0:
+    d[b_i] -= 1
+  else:
+    flag = False
+    break
+
+if flag:
+  print("Yes")
+else:
+  print("No")
