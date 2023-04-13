4d00dfd513554b4954469d713dfd4098275470d7
reo11 <reohirao116@gmail.com>
2023-04-14 00:53:56 +0900

    syojin

--- atcoder/ABC/abc201-300/abc266/f.py
+++ atcoder/ABC/abc201-300/abc266/f.py
@@ -1,8 +1,7 @@
 import sys
-from collections import defaultdict
+from collections import defaultdict, deque
 
-input = lambda: sys.stdin.readline().rstrip()
-sys.setrecursionlimit(20000000)
+input = sys.stdin.readline
 
 n = int(input())
 edges = defaultdict(lambda: [])
@@ -28,58 +27,62 @@ ans = []
 
 # 1. dfsで閉路を見つける
 # l: これまでの道のり
-# 次数で特定可能
-def dfs(l, current_edge_num, s):
-    if len(edges[current_edge_num]) == 1:
-        # 行き止まり
-        return None
-    for next_edge_num in edges[current_edge_num]:
-        if len(l) > 0 and l[-1] == next_edge_num:
-            continue
-        if next_edge_num in s:
-            ret_s = set()
-            for edge_i in reversed(l + [current_edge_num]):
-                ret_s.add(edge_i)
-                if edge_i == next_edge_num:
-                    break
-            return ret_s
-        if len(edges[next_edge_num]) <= 1:
-            continue
-        s_i = dfs(l + [current_edge_num], next_edge_num, s | {current_edge_num})
-        if s_i:
-            return s_i
-    return None
-loop_set = dfs([], 1, set())
-if not loop_set:
-    # RE対策？
-    loop_set = set()
+q = deque()
+q.append([-1, 1])
+l = deque()
+s = set()
+loop_set = set()
+while len(q) > 0:
+    pre, current_edge_num = q.popleft()
+    l.append(current_edge_num)
+    s.add(current_edge_num)
 
-# 2. loop_setにたどり着くまでグループ分け
-group_id = 0
-edge_groups = defaultdict(lambda: -1)
-group_members = defaultdict(lambda: set())
-
-for i in range(1, n + 1):
-    if edge_groups[i] != -1:
-        # 所属済み
+    if len(l) > 1 and len(edges[current_edge_num]) == 1:
+        # 行き止まりの場合、q[0]の値になるまで戻る
+        while True:
+            if l[-1] == q[0][0]:
+                break
+            else:
+                l.pop()
         continue
 
-    current_edge = i
-    q = [current_edge]
-    while len(q) > 0:
-        current_edge = q.pop()
-        if edge_groups[current_edge] != -1:
+    for next_edge_num in edges[current_edge_num]:
+        if len(l) > 1 and l[-2] == next_edge_num:
             continue
-        # 次の頂点もグループに入れる
-        edge_groups[current_edge] = group_id
-        group_members[group_id].add(current_edge)
-        for next_edge in edges[current_edge]:
-            if current_edge in loop_set and next_edge in loop_set:
-                continue
-            if edge_groups[next_edge] != -1:
-                continue
-            q.append(next_edge)
-    group_id += 1
+        if next_edge_num in s:
+            for edge_i in reversed(l):
+                loop_set.add(edge_i)
+                if edge_i == next_edge_num:
+                    break
+            q = deque()
+            break
+        q.appendleft([current_edge_num, next_edge_num])
+
+# 2. loop_setにたどり着くまでグループ分け
+edge_groups = [-1 for _ in range(n + 1)]
+group_members = [set() for _ in range(n + 1)]
+
+q = deque()
+for i in range(1, n + 1):
+    q.append([i, i])
+
+while len(q) > 0:
+    current_edge, current_group_id = q.popleft()
+    if edge_groups[current_edge] != -1:
+        continue
+    # 次の頂点もグループに入れる
+    edge_groups[current_edge] = current_group_id
+    group_members[current_group_id].add(current_edge)
+    for next_edge in edges[current_edge]:
+        if current_edge in loop_set and next_edge in loop_set:
+            continue
+        if edge_groups[next_edge] != -1:
+            continue
+        q.appendleft([next_edge, current_group_id])
+
+# print(loop_set)
+# print(edge_groups)
+# print(group_members)
 
 for x, y in xy:
     if edge_groups[x] != edge_groups[y]:
