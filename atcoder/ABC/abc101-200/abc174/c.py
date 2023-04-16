465dc8ae58b7c1933ee0cbbcd0c63280b0d5e093
reo11 <reohirao116@gmail.com>
2023-04-16 15:24:53 +0900

    ✨精進

--- atcoder/ABC/abc101-200/abc174/c.py
+++ atcoder/ABC/abc101-200/abc174/c.py
@@ -1,1 +1,11 @@
-k = int(input())
+mod = int(input())
+
+ans = -1
+num = 7
+for i in range(1, mod + 1):
+    if num % mod == 0:
+        ans = i
+        break
+    num = (num * 10) % mod
+    num = (num + 7) % mod
+print(ans)
