b2cb93e863659cb4d73f3872e6abd7d564d27392
reo11 <reohirao116@gmail.com>
2023-05-06 16:04:13 +0900

    :sparkles:

--- atcoder/typical90/src/bin/018.rs
+++ atcoder/typical90/src/bin/018.rs
@@ -2,12 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-
+        t: f64,
+        l: f64,
+        x: f64,
+        y: f64,
+        q: usize,
+        ask: [f64; q],
     }
-
-    let mut ans = 0;
-
-
-    println!("{}", ans);
+    let l = l * 0.5;
+    for e in ask {
+        let rad = e / t * 2.0 * std::f64::consts::PI;
+        let p = -l * rad.sin();
+        let q = l - l * rad.cos();
+        let w = (x.powi(2) + (y - p).powi(2)).sqrt();
+        let ans = q.atan2(w) / 2.0 / std::f64::consts::PI * 360.0;
+        println!("{:.8}", ans);
+    }
 }
--- atcoder/typical90/src/bin/019.rs
+++ atcoder/typical90/src/bin/019.rs
@@ -3,10 +3,13 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-
+        a: [i64; n],
     }
 
     let mut ans = 0;
+
+    // 区間DP?
+    // DP[i][j] = 区間[i, j)の最小コスト
 
 
     println!("{}", ans);
