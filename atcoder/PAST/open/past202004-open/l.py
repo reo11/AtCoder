319460765780777491afbf1f01dbd271c046c351
reo11 <reohirao116@gmail.com>
2023-06-03 19:51:20 +0900

    :run: shojin

--- atcoder/PAST/open/past202004-open/l.py
+++ atcoder/PAST/open/past202004-open/l.py
@@ -1,3 +1,14 @@
+import math
+
 n, k, d = map(int, input().split())
 a = list(map(int, input().split()))
 
+# 不可能な場合
+if math.ceil(n / d) < k:
+    print(-1)
+    exit()
+else:
+    ans = []
+    for i in range(k):
+        ans.append(a[i * d])
+    print(*ans)
--- atcoder/PAST/past202004-open/m.py
+++
@@ -1,6 +0,0 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
-d, l, n = map(int, input().split())
-c = list(map(int, input().split()))
-kft = [list(map(int, input().split())) for _ in range(n)]
