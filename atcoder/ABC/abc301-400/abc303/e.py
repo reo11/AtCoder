178ec0b3b3d43fb6d6848c359c2053634ba1aabb
reo11 <reohirao116@gmail.com>
2023-07-02 20:49:36 +0900

    🔰 abc303-e

--- atcoder/ABC/abc301-400/abc303/e.py
+++ atcoder/ABC/abc301-400/abc303/e.py
@@ -1,59 +1,20 @@
-from collections import defaultdict, deque
+N = int(input())
+dims = [0] * N
+for _ in range(N-1):
+    a, b = map(int,input().split())
+    dims[a - 1] += 1
+    dims[b - 1] += 1
 
-n = int(input())
-edges = defaultdict(lambda: set())
-for i in range(n - 1):
-    u, v = map(int, input().split())
-    edges[u].add(v)
-    edges[v].add(u)
-
-q = deque([])
-for edge, vs in edges.items():
-    if len(vs) == 1:
-        q.append(edge)
-
-
-def delete_edge(u, v):
-    edges[u].discard(v)
-    edges[v].discard(u)
-
+cnt = [0] * N
+for x in dims:
+    cnt[x] += 1
 
 ans = []
-# 次数から端を探して、その連結成分を星の中心と考えると良い
-while True:
-    # print(ans, q, edges)
-    if len(q) > 0:
-        v = q.popleft()
-    else:
-        count_multi_edge = 0
-        for edge, vs in edges.items():
-            if len(vs) == 1:
-                q.append(edge)
-            elif len(vs) > 1:
-                count_multi_edge += 1
+vs = 0
+for i, x in enumerate(cnt):
+    if i >= 3:
+        vs += (i + 1) * x
+        ans += [i] * x
 
-        if count_multi_edge == 0:
-            break
-        if len(q) == 0:
-            break
-        else:
-            v = q.popleft()
-    if len(edges[v]) == 0:
-        continue
-    center = list(edges[v])[0]
-    center_edges = []
-    for edge in edges[center]:
-        center_edges.append(edge)
-    if len(center_edges) <= 1:
-        continue
-    ans.append(len(center_edges))
-    # 星の頂点として使用した分削除
-    for u in center_edges:
-        delete_edge(u, center)
-        next_us = list(edges[u])
-        for next_u in next_us:
-            if len(edges[next_u]) <= 2:
-                delete_edge(u, next_u)
-
-ans.sort()
-print(*ans, sep=" ")
+ans = [2] * ((N - vs) // 3) + ans
+print(*ans)
