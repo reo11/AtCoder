c38c5316f7fd47b6500465c80faa8207d3b0312d
reo11 <reohirao116@gmail.com>
2023-07-02 20:49:48 +0900

    🔰 abc305-e

--- atcoder/ABC/abc301-400/abc305/e.py
+++ atcoder/ABC/abc301-400/abc305/e.py
@@ -1,8 +1,9 @@
 import heapq
 from collections import defaultdict
-from sys import stdin
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 
-input = stdin.readline
+MAX_N = 3 * 10**5
 
 n, m, k = map(int, input().split())
 ab = [list(map(int, input().split())) for _ in [0] * m]
@@ -12,27 +13,37 @@ for a, b in ab:
     edges[a].append([b, 1])
     edges[b].append([a, 1])
 for p, h in ph:
-    edges[n + 1].append([p, n - h])
-    edges[p].append([n + 1, n - h])
+    edges[n + 1].append([p, MAX_N - h])
 
 # 頂点n+1から距離n以下の点を列挙
-ans = []
+ans = set()
 visited = defaultdict(lambda: False)
+visited_count = 0
+min_costs = defaultdict(lambda: float("inf"))
 q = []
 heapq.heapify(q)
-heapq.heappush(q, [0, n + 1])
+heapq.heappush(q, (0, n + 1))
 
-while len(q) > 0:
+while q:
+    # print(q)
     cost, edge = heapq.heappop(q)
     visited[edge] = True
+    min_costs[edge] = min(min_costs[edge], cost)
     # print(cost, edge)
-    if cost <= n:
-        ans.append(edge)
+    if cost <= MAX_N and edge not in ans:
+        ans.add(edge)
+    else:
+        continue
     for next_edge, next_cost in edges[edge]:
         if visited[next_edge]:
             continue
-        visited[next_edge] = True
-        heapq.heappush(q, [cost + next_cost, next_edge])
+        if cost + next_cost > MAX_N:
+            continue
+        if cost + next_cost >= min_costs[next_edge]:
+            continue
+        else:
+            min_costs[next_edge] = cost + next_cost
+            heapq.heappush(q, (cost + next_cost, next_edge))
 
 ans = list(set(ans) - set([n + 1]))
 ans = sorted(ans)
