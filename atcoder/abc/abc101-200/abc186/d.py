042c4b83bea95a85c201d1f0c727e569d316aec3
reo11 <reohirao116@gmail.com>
2023-07-17 15:28:27 +0900

    🔰

--- atcoder/abc/abc101-200/abc186/d.py
+++ atcoder/abc/abc101-200/abc186/d.py
@@ -1,17 +1,13 @@
-def base10int(num_10, n):
-    str_n = []
-    while num_10:
-        if num_10 % n >= 10:
-            return -1
-        str_n.append(num_10 % n)
-        num_10 //= n
-    return str_n[::-1]
-
-
 n = int(input())
-
+a = list(map(int, input().split()))
+a.sort(reverse=True)
 ans = 0
+
+# ai
 for i in range(1, n + 1):
-    if not "7" in str(i) and not "7" in base10int(8, i):
-        ans += 1
+    ans += (n - i) * a[i - 1]
+
+# -aj
+for i in range(1, n + 1):
+    ans -= (i - 1) * a[i - 1]
 print(ans)
