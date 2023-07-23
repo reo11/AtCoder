bb279eb491830c5580ab84b6f00aa03dfba0d0ce
reo11 <reohirao116@gmail.com>
2023-07-23 16:11:35 +0900

    fixed

--- atcoder/abc/abc301-400/abc311/c.py
+++ atcoder/abc/abc301-400/abc311/c.py
@@ -13,27 +13,31 @@ visited = [False for _ in range(n + 1)]
 q = deque()
 
 num = 1
+
 while num <= n:
     # 探索する頂点を決める
+    path = [-1 for _ in range(n + 1)]
     while num <= n:
         if visited[num]:
             num += 1
             continue
         else:
-            q.append([num, []])
+            q.append([num, 0])
             num += 1
             break
     while q:
-        v, path = q.popleft()
+        v, number = q.popleft()
+        path[number] = v
         visited[v] = True
         for u in edges[v]:
             if not visited[u]:
-                q.append([u, path + [v]])
+                q.appendleft([u, number + 1])
+                visited[u] = True
             else:
                 ans = []
-                for ans_v in reversed(path + [v]):
-                    ans.append(ans_v)
-                    if ans_v == u:
+                for path_i in reversed(range(number + 1)):
+                    ans.append(path[path_i])
+                    if path[path_i] == u:
                         ans = ans[::-1]
                         print(len(ans))
                         print(" ".join(map(str, ans)))
