24942b65bd06e927134cc04e1fa6c21b2b04e75e
reo11 <reohirao116@gmail.com>
2023-10-14 20:56:16 +0900

    ⛰️ arc166

---
+++ atcoder/ARC/arc166/a.py
@@ -0,0 +1,75 @@
+from collections import defaultdict, deque
+t = int(input())
+
+def solve(n, x, y):
+    # x, y はABCからなる文字列
+    # A...B...の順に変形する
+    # y_tmpと同じになるように後ろから決めていく
+    x = list(x)
+    y = list(y)
+
+    # Cのロックと置き換えを最初に行う
+    # Cは境界になる
+    counter_x = defaultdict(lambda: 0)
+    counter_y = defaultdict(lambda: 0)
+    xc_queue = deque()
+    for i, (xi, yi) in enumerate(zip(x, y)):
+        if yi == "C" and xi == "C":
+            # Bの不足分を後ろから埋めていく
+            # そのほかのCは全てAに置き換える
+            while xc_queue:
+                idx = xc_queue.pop()
+                if counter_y["B"] > counter_x["B"]:
+                    x[idx] = "B"
+                    counter_x["B"] += 1
+                    counter_x["C"] -= 1
+                else:
+                    x[idx] = "A"
+                    counter_x["A"] += 1
+                    counter_x["C"] -= 1
+            if counter_x["A"] != counter_y["A"] or counter_x["B"] != counter_y["B"]:
+                return "No"
+            counter_x = defaultdict(lambda: 0)
+            counter_y = defaultdict(lambda: 0)
+        elif yi == "C" and xi != "C":
+            return "No"
+        else:
+            counter_x[xi] += 1
+            counter_y[yi] += 1
+            if xi == "C":
+                xc_queue.append(i)
+    while xc_queue:
+        idx = xc_queue.pop()
+        if counter_y["B"] > counter_x["B"]:
+            x[idx] = "B"
+            counter_x["B"] += 1
+            counter_x["C"] -= 1
+        else:
+            x[idx] = "A"
+            counter_x["A"] += 1
+            counter_x["C"] -= 1
+    if counter_x["A"] != counter_y["A"] or counter_x["B"] != counter_y["B"]:
+        return "No"
+
+    # Bの数を数えて超えなければok
+    counter = [0, 0]
+    for xi, yi in zip(x, y):
+        if xi == "C" or yi == "C":
+            counter = [0, 0]
+            continue
+        if yi == "B":
+            counter[1] += 1
+        if xi == "B":
+            counter[0] += 1
+        if counter[0] > counter[1]:
+            return "No"
+
+    return "Yes"
+
+
+
+ans = []
+for _ in range(t):
+    n, x, y = input().split()
+    ans.append(solve(n, x, y))
+print(*ans, sep="\n")
---
+++ atcoder/ARC/arc166/b.py
@@ -0,0 +1,76 @@
+import itertools
+import sys
+import pypyjit
+from functools import lru_cache
+from collections import defaultdict
+
+pypyjit.set_param('max_unroll_recursion=-1')
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(20000000)
+
+@lru_cache(maxsize=None)
+def gcd(a: int, b: int) -> int:
+    # 最大公約数
+    # (12, 18) -> 6
+    while b:
+        a, b = b, a % b
+    return a
+
+@lru_cache(maxsize=None)
+def lcm(x: int, y: int) -> int:
+    # 最小公倍数
+    # (12, 18) -> 36
+    return (x * y) // gcd(x, y)
+
+
+INF = float("inf")
+n, a, b, c = map(int, input().split())
+A = list(map(int, input().split()))
+abc = [a, b, c]
+
+# 割り当て優先度を全探索する
+# 3 * 2 * 1 = 6通り
+
+# print(perm)
+patterns = [["a", "b", "c"], ["ab", "c"], ["ac", "b"], ["bc", "a"], ["abc"]]
+mod_abc = defaultdict(lambda: [])
+
+ab = lcm(a, b)
+bc = lcm(b, c)
+ac = lcm(a, c)
+abc = lcm(ab, c)
+for i in range(n):
+    ai = A[i]
+    mod_abc["a"].append([a - (ai % a), i])
+    mod_abc["b"].append([b - (ai % b), i])
+    mod_abc["c"].append([c - (ai % c), i])
+    mod_abc["ab"].append([ab - (ai % ab), i])
+    mod_abc["ac"].append([ac - (ai % ac), i])
+    mod_abc["bc"].append([bc - (ai % bc), i])
+    mod_abc["abc"].append([abc - (ai % abc), i])
+
+for k in mod_abc.keys():
+    mod_abc[k] = sorted(mod_abc[k], key=lambda x: x[0])[:3]
+
+ans = INF
+for pattern_orders in patterns:
+    if n < len(pattern_orders):
+        continue
+    candidate = []
+    for pattern_i in pattern_orders:
+        candidate.append(mod_abc[pattern_i][:len(pattern_orders)])
+    for candidatei in itertools.product(*candidate):
+        used_i = set()
+        cost = 0
+        flag = True
+        for costi, i in candidatei:
+            if i in used_i:
+                flag = False
+                break
+            cost += costi
+            used_i.add(i)
+
+        if flag:
+            ans = min(ans, cost)
+
+print(ans)
