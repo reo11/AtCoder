22b0887d319b3d72a3dd3f91a9fb77e9e5aa0f5b
reo11 <reohirao116@gmail.com>
2023-10-31 00:06:42 +0900

    wakaran

---
+++ atcoder/abc/abc101-200/ABC149/e.py
@@ -0,0 +1,19 @@
+import bisect
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+
+left = 0
+right = 10 ** 12
+while right - left > 1:
+    mid = (left + right) // 2
+    cnt = 0
+    for i in range(n):
+        cnt += bisect.bisect_left(a, mid - a[i])
+    print(mid, cnt)
+    if cnt >= m:
+        right = mid
+    else:
+        left = mid
+print(left, right)
+# wakaran
